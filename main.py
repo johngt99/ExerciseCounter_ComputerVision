7d0ce732d2b0e0048a77fa73695288a0ea299e7a
JoaoTeixeira99 <joaoggt.99@gmail.com>
2023-02-25 23:54:01 +0000

    'Base-files'

---
+++ main.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+# Define our different screens
+class MainPage(Screen):
+    pass
+
+class SecondWindow(Screen):
+    pass
+
+class ThirdWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+# Designate our .kv file > Design file
+kv = Builder.load_file('styles.kv')
+
+class AwesomeApp(App):
+    def build(self):
+        return kv
+    
+if __name__ == '__main__':
+    AwesomeApp().run()
+
